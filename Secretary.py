2188d8e9a78936ddf6eb7a431c8ef563b1e5adae
Anton Chivchalov <antorix@gmail.com>
2019-03-19 13:57:46 +0300

    Add files via upload

--- Secretary.py
+++ Secretary.py
@@ -1,19 +1,20 @@
-mypath1="D:\Мои документы\Отчеты\Возвещатели" # папки бланков S-21, пропишите здесь полные пути
-mypath2="D:\Мои документы\Отчеты\Возвещатели некрещеные"
-mypath3="D:\Мои документы\Отчеты\Пионеры"
-mypath4="D:\Мои документы\Отчеты\Пионеры подсобные"
-PDFViwer="C:\Program Files\Tracker Software\PDF Viewer\PDFXCview.exe" # полный путь к PDF-читалке
+mypath1="S:\\Папки\\70927 Минск, Северо-Западное\\S-21 Карточки собрания для отчетов возвещателей\\Возвещатели\\" # папки бланков S-21, пропишите здесь полные пути
+mypath2="S:\\Папки\\70927 Минск, Северо-Западное\\S-21 Карточки собрания для отчетов возвещателей\\Возвещатели некрещеные\\"
+mypath3="S:\\Папки\\70927 Минск, Северо-Западное\\S-21 Карточки собрания для отчетов возвещателей\\Пионеры\\"
+mypath4="S:\\Папки\\70927 Минск, Северо-Западное\\S-21 Карточки собрания для отчетов возвещателей\\Пионеры подсобные\\"
 Filename="publishers.txt" # файл с возвещателями
 Delimeter="\t" # разделитель данных при вводе
 
-import keyboard
 import webbrowser
 from os import path, listdir
 from os.path import isfile, join
-import xlrd
 import glob
-RowCount=0
-Values=["", "", "", "", ""]
+try:
+    import xlrd
+    import keyboard
+except:
+    print("Нет необходимых библиотек. Введите \"?\" для просмотра справки.")
+RowCount=Cycles=0
 
 def load():
     """Загрузка базы возвещателей из файла"""
@@ -80,6 +81,7 @@ def search(input, pub):
     """Поиск возвещателя и ввод его данных"""
     global Values
     global RowCount
+    global Cycles
     for i in range(6):
         if input[i]=="":
             input[i]="0.0"
@@ -94,9 +96,10 @@ def search(input, pub):
             line=i
             count+=1
     if count==0:
-        print("Ничего не найдено.")
+        print("Ничего не найдено. Введите \"?\" для просмотра справки.")
     elif count==1:
         if input[1]==None:
+            print("[%s\t%s\t%s\t%s\t%s]" % (pub[line][1], pub[line][2], pub[line][3], pub[line][4], pub[line][5]))
             webbrowser.open(pub[line][0])
             return
         else:
@@ -109,6 +112,7 @@ def search(input, pub):
             webbrowser.open(pub[line][0])
             Values=[input[1].strip(), input[2].strip(), input[3].strip(), input[4].strip(), input[5].strip()]
             RowCount=0
+            Cycles=5
             keyboard.wait('ctrl+w')
     else:
         print("Найдено %s, конкретизируйте." % count)   
@@ -121,44 +125,79 @@ def recreate():
         files = files + [mypath2+f for f in listdir(mypath2) if isfile(join(mypath2, f))]        
         files = files + [mypath3+f for f in listdir(mypath3) if isfile(join(mypath3, f))]
         files = files + [mypath4+f for f in listdir(mypath4) if isfile(join(mypath4, f))]
-        print("Сброс файла данных и перезагрузка из папок…")
     except:
-        print("Не найдены нужные папки, проверьте настройки.")
+        print("Не найдены нужные папки, проверьте настройки. Введите \"?\" для просмотра справки.")
         return
     files.sort(key=lambda x: x[0])    
     with open(Filename, "w", encoding="utf-8") as datafile:
         for row in files:
             datafile.write(row+"\n")
 
-def stats(pub):
+def stats(pub, mode=1):
     """Подсчет и вывод статистики для ввода на jw.org (команда =)"""
-    countP=countP_Hours=countP_Ret=countP_St=countAP=countAP_Hours=countAP_Ret=countAP_St=countRP=countRP_Hours=countRP_Ret=countRP_St=0.0
+    global Values
+    global RowCount
+    global Cycles
+    countP=countP_Pub=countP_Vid=countP_Hours=countP_Ret=countP_St=countAP=countAP_Pub=countAP_Vid=countAP_Hours=countAP_Ret=countAP_St=countRP=countRP_Pub=countRP_Vid=countRP_Hours=countRP_Ret=countRP_St=0.0
     for row in pub:
         if("подсобные\\" in row[0] and row[3]!=""):
             countAP+=1
+            countAP_Pub+=int(row[1])
+            countAP_Vid+=int(row[2])
             countAP_Hours+=float(row[3])
             countAP_Ret+=int(row[4])
             countAP_St+=int(row[5])
         elif("\\Пионеры\\" in row[0] and row[3]!=""):
             countRP+=1
+            countRP_Pub+=int(row[1])
+            countRP_Vid+=int(row[2])
             countRP_Hours+=float(row[3])
             countRP_Ret+=int(row[4])
             countRP_St+=int(row[5])
         elif("\\Возвещатели\\" in row[0] and row[3]!=""):
             countP+=1
+            countP_Pub+=int(row[1])
+            countP_Vid+=int(row[2])
             countP_Hours+=float(row[3])
             countP_Ret+=int(row[4])
             countP_St+=int(row[5])
-    print("Возвещатели:\nЧисло сдавших отчет: %d\nЧасы: %.1f\nПовторные посещения: %d\nИзучения Библии: %d\n" % (countP, countP_Hours, countP_Ret, countP_St))
-    print("Подсобные пионеры:\nЧисло сдавших отчет: %d\nЧасы: %.1f\nПовторные посещения: %d\nИзучения Библии: %d\n" % (countAP, countAP_Hours, countAP_Ret, countAP_St))
-    print("Пионеры:\nЧисло сдавших отчет: %d\nЧасы: %.1f\nПовторные посещения: %d\nИзучения Библии: %d\n" % (countRP, countRP_Hours, countRP_Ret, countRP_St))
-    print("Активные возвещатели: %d" % len(pub))
+    if mode==1: # показ статистики
+        print("Возвещатели:\nЧисло сдавших отчет: %d\nЧасы: %.1f\nПовторные посещения: %d\nИзучения Библии: %d\n" % (countP, countP_Hours, countP_Ret, countP_St))
+        print("Подсобные пионеры:\nЧисло сдавших отчет: %d\nЧасы: %.1f\nПовторные посещения: %d\nИзучения Библии: %d\n" % (countAP, countAP_Hours, countAP_Ret, countAP_St))
+        print("Пионеры:\nЧисло сдавших отчет: %d\nЧасы: %.1f\nПовторные посещения: %d\nИзучения Библии: %d\n" % (countRP, countRP_Hours, countRP_Ret, countRP_St))
+        print("Активные возвещатели: %d" % len(pub))
+    elif mode==2: # автоввод статистики на сайте
+        print("Жду окончания ввода...")
+        webbrowser.open("https://apps.jw.org/U_A1B222FF-99B5-4021-8B12-BE9BAFE86234_ENTFLDSVC")
+        Values=[    str(int(countP)),  str(int(countP_Hours)),  str(int(countP_Ret)),  str(int(countP_St)),
+                    str(int(countAP)), str(int(countAP_Hours)), str(int(countAP_Ret)), str(int(countAP_St)),
+                    str(int(countRP)), str(int(countRP_Hours)), str(int(countRP_Ret)), str(int(countRP_St))     ]
+        Cycles=len(Values)
+        RowCount=0
+        keyboard.wait('ctrl+w')        
+    elif mode==3: # автоввод статистики для РН, адаптированной под бланк S-21 (4 в 1)
+        print("Открываю файл.\nЖду окончания ввода...")
+        webbrowser.open("S:\Папки\70927 Минск, Северо-Западное\S-21 Карточки собрания для отчетов возвещателей\S-21 для РН 2019.pdf")
+        Values=[    str(int(countRP_Pub)), str(int(countRP_Vid)), str(int(countRP_Hours)), str(int(countRP_Ret)), str(int(countRP_St)), str(int(countRP)),
+                    str(int(countP_Pub)),  str(int(countP_Vid)),  str(int(countP_Hours)),  str(int(countP_Ret)),  str(int(countP_St)),  str(int(countP)),
+                    str(int(countAP_Pub)), str(int(countAP_Vid)), str(int(countAP_Hours)), str(int(countAP_Ret)), str(int(countAP_St)), str(int(countAP))]
+        Cycles=len(Values)
+        RowCount=0
+        keyboard.wait('ctrl+w')
+        
+def extractname(string):
+    """Сокращение имени файла возвещателя до более удобного"""
+    try:
+        return string[string.index("возвещателей\\")+13 : string.index("S-21.pdf")].strip()
+    except:
+        return string
 
 def reprow():
     """Подстановка данных в буфер обмена"""
     global RowCount
     global Values
-    if RowCount<5:
+    global Cycles
+    if RowCount<Cycles:
         if Values[RowCount]!="0":
             keyboard.write(Values[RowCount])
         else:
@@ -191,12 +230,7 @@ def remaining(pub):
             count+=1
     print("Не сдали отчеты: %d" % count)
 
-def extractname(string):
-    #"""Сокращение имени файла возвещателя до более удобного"""
-    #return string[string.index("возвещателей\\")+13 : string.index("S-21.pdf")].strip()
-    return string
-
-webbrowser.open(PDFViwer)
+print("Ищу базу данных…")
 pub=load()
 if pub==None:
     print("Создаю файл из папок…")
@@ -211,10 +245,15 @@ while 1:
     if command=="?": # открытие справки в онлайне
         webbrowser.open("https://github.com/antorix/secretary/blob/master/readme.md")
     elif command=="СБРОС":
+        print("Сброс файла данных и перезагрузка из папок…")
         recreate()
         pub=load()        
     elif command=="=":
         stats(pub)
+    elif command=="=jw":
+        stats(pub, mode=2)
+    elif command=="=co":
+        stats(pub, mode=3)
     elif command=="\\":
         webbrowser.open(Filename)
     elif command=="+":
@@ -226,4 +265,4 @@ while 1:
             result1=fetch(command)
             result2=search(result1, pub)
         except:
-            print("Команда непонятна, попробуйте еще (введите \"?\" для показа справки).")
+            print("Команда непонятна, попробуйте еще (введите \"?\" для просмотра справки).")
